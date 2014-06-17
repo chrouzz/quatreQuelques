51acb948e4e86d8fac73340c0efe58324ee5a690
leCassis <coderman48hotmail.com>
2014-06-17 13:33:06 +0900

    Prout

---
+++ api/models/FavoriteMember.js
@@ -0,0 +1,24 @@
+/**
+ * FavoriteMember
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+  attributes: {
+  	userId: {
+      type: 'integer'
+    },
+    favoriteMemberId: {
+      type: 'integer'
+    },
+  	/* e.g.
+  	nickname: 'string'
+  	*/
+    
+  }
+
+};
