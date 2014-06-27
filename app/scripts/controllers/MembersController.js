a481a2188d2fb76dc3b172c6dc7eece871b1873d
leCassis <coderman48hotmail.com>
2014-06-27 12:37:07 +0900

    Prout

--- app/scripts/controllers/MembersController.js
+++ app/scripts/controllers/MembersController.js
@@ -3,6 +3,7 @@
 
 angular.module('barmaddenApp').controller('MembersController', function ($scope, $resource) {
   var Members = $resource('/user');
+  console.log(Members);
   $scope.members  = Members.query();
 
   $scope.message = "Members";
